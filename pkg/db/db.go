ca372810ef2f28ea42c2727f2569b5df5494368e
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2025-02-01 19:17:00 +0300

    Добавлены опции для подключения к БД

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,10 +4,15 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"poymanov/todo/config"
+	"time"
 )
 
 func NewDb(conf *config.Config) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(conf.DB.DbConnectionAsString()), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(conf.DB.DbConnectionAsString()), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
 
 	if err != nil {
 		panic(err)
